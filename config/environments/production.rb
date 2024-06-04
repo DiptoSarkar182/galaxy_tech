f4f889533c1a532a6dc601c6488a68cfb1d737b3
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-06-04 17:41:46 +0600

    added send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,10 +109,11 @@ Rails.application.configure do
   # }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.resend.com',
-    port: 465,
-    user_name: 'resend',
-    password: 're_j3746QC8_C2L8SJUGXkGzWhQkgCEbcS7n',
-    tls: true
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 end
--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,9 +1,9 @@
-require 'money/bank/open_exchange_rates_bank'
-
-MoneyRails.configure do |config|
-  oxr = Money::Bank::OpenExchangeRatesBank.new
-  oxr.app_id = ENV['OPEN_EXCHANGE_RATES_APP_ID']
-  oxr.cache = Rails.root.join('tmp', 'exchange_rates.json')
-  oxr.update_rates
-  config.default_bank = oxr
-end
+# require 'money/bank/open_exchange_rates_bank'
+#
+# MoneyRails.configure do |config|
+#   oxr = Money::Bank::OpenExchangeRatesBank.new
+#   oxr.app_id = ENV['OPEN_EXCHANGE_RATES_APP_ID']
+#   oxr.cache = Rails.root.join('tmp', 'exchange_rates.json')
+#   oxr.update_rates
+#   config.default_bank = oxr
+# end
